f4023fe9d6b34f4c624e7da1fedbc1ade63604ff
Ritesh Patel <patel2000ritesh@gmail.com>
2020-06-15 07:39:40 -0400

    Updated String Rotation

--- src/Chapter1/String_Rotation.java
+++ src/Chapter1/String_Rotation.java
@@ -1,4 +1,10 @@
 package Chapter1;
 
 public class String_Rotation {
+
+    public static void main(String[] args) {
+        String a = "Hello";
+        String b = "He";
+        System.out.println(a.contains(b));
+    }
 }
